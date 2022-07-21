8a11de73232017dbfefd0d9347e8236bad50d3a2
Juan Debenedetti <juandebenedetti94@gmail.com>
2022-07-21 09:14:49 -0300

    Update

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,8 +8,8 @@ import java.awt.image.*;
 /**
  * Se encarga de manejar la logica del juego a travez del game loop que actua como subproceso.
  * <p>
- * El game loop cuenta con las variables ticks y fps. Los ticks indican cuantas veces el motor del juego se repite en
- * un segundo y los FPS señalan cuantas veces el motor de renderizado renderizara una nueva imagen en un segundo.
+ * Los nanosegundos no depende del SO, sino del microprocesador y se miden tomando como referencia los ciclos de reloj
+ * del procesador.
  */
 
 public class Game implements Runnable {
@@ -45,10 +45,13 @@ public class Game implements Runnable {
 		init();
 
 		// https://stackoverflow.com/questions/26838286/delta-time-getting-60-updates-a-second-in-java
+		// https://stackoverflow.com/questions/57710138/why-gameloops-render-more-times-than-updating#:~:text=A%20tick%20is%20whenever%20game,to%20a%20redstone%20circuit%20updating.
 		int fps = 60; // Cantidad de veces que llama a tick y render en cada segundo
 		/* Calcula la cantidad de tiempo en nanosegundos que hay entre cada frame. El tiempo se mide en nanosegundos
 		 * (1_000_000_000 nanosegundos equivalen a 1 segundo) ya que es una unidad mucho mas especifica para la CPU.
-		 * TODO El nombre nsBetweenTicks tiene mas sentido. */
+		 * El fin de esta variable es saber cuantos ns tienen que transcurrir para actualizar el frame 60 veces en un
+		 * segundo.
+		 * TODO El nombre nsBetweenTicks o nsPerTick tienen mas sentido. */
 		double timePerTick = 1e9 / fps;
 		double delta = 0;
 		long now, lastTime = System.nanoTime(); // Devuelve la hr actual de la PC en nanosegundos
@@ -62,8 +65,11 @@ public class Game implements Runnable {
 			 * cantidad maxima de tiempo que podemos tener para llamar a tick y render. Ahora con el valor delta
 			 * calculado, el programa sabe cuanto tiempo tiene hasta que tenga que llamar a los metodos tick y render
 			 * nuevamente. */
-			// Acumula la diferencia entre la hr actual y ultima sobre timePerTick
+			/* Acumula la diferencia entre la hr actual y ultima sobre timePerTick. Esto hace posible que se ejecute
+			 * en cualquier PC a la misma velocidad. */
 			delta += (now - lastTime) / timePerTick;
+
+			// 86470255333400 - 86470255330500 = 2900 / 16.666.666 =
 
 			System.out.println("now = " + now + " ____ lastTime " + lastTime);
 
@@ -83,11 +89,11 @@ public class Game implements Runnable {
 				System.out.println("Se actualizaron 60 frames en un segundo!");
 				System.out.println("La posicion x del frame esta en " + x);
 				ticks = 0;
-				try {
+				/* try {
 					Thread.sleep(5000);
 				} catch (InterruptedException e) {
 					throw new RuntimeException(e);
-				}
+				} */
 			}
 
 
