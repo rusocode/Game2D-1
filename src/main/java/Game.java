a60388b1dcdbb8b689a76eea5d111614c1948e36
Juan Debenedetti <juandebenedetti94@gmail.com>
2022-07-21 20:16:58 -0300

    Update

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -26,7 +26,7 @@ public class Game implements Runnable {
 	private final String title;
 	private final int width;
 	private final int height;
-	private static final int FPS = 1;
+	private static final int FPS = 60;
 	private int x;
 	private boolean running;
 
@@ -44,44 +44,13 @@ public class Game implements Runnable {
 
 		init();
 
-		// https://stackoverflow.com/questions/26838286/delta-time-getting-60-updates-a-second-in-java
-		// https://stackoverflow.com/questions/57710138/why-gameloops-render-more-times-than-updating#:~:text=A%20tick%20is%20whenever%20game,to%20a%20redstone%20circuit%20updating.
-		int fps = 60; // Cantidad de veces que llama a tick y render en cada segundo
-		/* Calcula la cantidad de tiempo en nanosegundos que hay entre cada frame. El tiempo se mide en nanosegundos
-		 * (1_000_000_000 nanosegundos equivalen a 1 segundo) ya que es una unidad mucho mas especifica para la CPU.
-		 * El fin de esta variable es saber cuantos ns tienen que transcurrir para actualizar el frame 60 veces en un
-		 * segundo.
-		 * TODO El nombre nsBetweenTicks o nsPerTick tienen mas sentido. */
-		double timePerTick = 1e9 / fps;
-		double delta = 0;
-		long now, lastTime = System.nanoTime(); // Devuelve la hr actual de la PC en nanosegundos
 		int ticks = 0;
 
 		while (running) {
 
-			now = System.nanoTime();
-
-			/* Obtiene el tiempo transcurrido entre cada frame renderizado. Luego divide esa cantidad de tiempo por la
-			 * cantidad maxima de tiempo que podemos tener para llamar a tick y render. Ahora con el valor delta
-			 * calculado, el programa sabe cuanto tiempo tiene hasta que tenga que llamar a los metodos tick y render
-			 * nuevamente. */
-			/* Acumula la diferencia entre la hr actual y ultima sobre timePerTick. Esto hace posible que se ejecute
-			 * en cualquier PC a la misma velocidad. */
-			delta += (now - lastTime) / timePerTick;
-
-			// 86470255333400 - 86470255330500 = 2900 / 16.666.666 =
-
-			System.out.println("now = " + now + " ____ lastTime " + lastTime);
-
-			lastTime = now;
-
-			/* Si el delta es mayor o igual a 1 segundo, significa que tiene que llamar a tick y render para lograr 60
-			 * frames por segundo. Esto significa que cada vez que se llama a tick(), el programa esperara 16666666
-			 * nanosegundos antes de volver a llamar a tick(). */
-			if (delta >= 1) {
+			if (timer.check()) {
 				tick();
 				render();
-				delta--;
 				ticks++;
 			}
 
@@ -89,13 +58,7 @@ public class Game implements Runnable {
 				System.out.println("Se actualizaron 60 frames en un segundo!");
 				System.out.println("La posicion x del frame esta en " + x);
 				ticks = 0;
-				/* try {
-					Thread.sleep(5000);
-				} catch (InterruptedException e) {
-					throw new RuntimeException(e);
-				} */
 			}
-
 
 		}
 
--- src/main/java/util/FpsTimer.java
+++ src/main/java/util/FpsTimer.java
@@ -1,20 +1,50 @@
 package util;
 
+/**
+ * El tiempo se mide en nanosegundos ya que es una unidad mucho mas especifica para la CPU.
+ * Hay 1000000000 nanosegundos en un segundo, lo que significa que la diferencia de tiempo por llamada de tick()
+ * es igual a 1000000000/60. Que es aproximadamente 16666666 nanosegundos. Esto significa que cada vez que se
+ * llama a tick(), el programa esperara 16666666 nanosegundos antes de volver a llamar a tick().
+ * Lo que debes hacer es encontrar el tiempo entre el frame actual y el ultimo frame en nanosegundos. Este
+ * valor dividido por el timestep (16666666) le dara un porcentaje decimal de cuanto ha pasado del tiempo
+ * necesario. Si agrega este porcentaje decimal a la variable delta, cuando la variable delta >= 1, ha pasado
+ * 1/60 de segundo, lo que significa que el programa ahora puede llamar a tick().
+ * <br>
+ * ¿Que es un timestep?
+ * "step" es un proceso de calculo del siguiente estado del sistema. "timestep" es el intervalo de tiempo durante el
+ * cual la simulacion progresara durante el siguiente "step".
+ *
+ * <a href="https://stackoverflow.com/questions/26838286/delta-time-getting-60-updates-a-second-in-java">...</a>
+ * <a href="https://stackoverflow.com/questions/57710138/why-gameloops-render-more-times-than-updating#:~:text=A%20tick%20is%20whenever%20game,to%20a%20redstone%20circuit%20updating">...</a>.
+ */
+
 public class FpsTimer {
 
-	private final double timePerTick;
-	private long lastTime;
+	private final double nsBetweenTicks;
+	private double delta;
+	private long lastTime, now;
 
 	public FpsTimer(int fps) {
-		timePerTick = 1e9 / fps;
+		nsBetweenTicks = 1e9 / fps;
+		// Obtiene el tiempo del ultimo frame en nanosegundos
 		lastTime = System.nanoTime();
 	}
 
 	public boolean check() {
-		long now = System.nanoTime();
-		double delta = now - lastTime;
 
-		if (delta >= timePerTick) {
+		now = System.nanoTime();
+
+		/* Calcula el tiempo transcurrido entre cada frame renderizado, que se obtiene restando el tiempo del primer
+		 * y ultimo frame. Luego divide la diferencia en nsBetweenTicks (cantidad maxima de tiempo que podemos tener
+		 * para llamar a tick y render) para obtener el tiempo en nanosegundos. Ahora con el valor delta calculado,
+		 * el programa sabe cuanto tiempo tiene hasta que tenga que llamar a los metodos tick y render nuevamente.
+		 * Esto hace posible que se ejecute en cualquier PC a la misma velocidad. */
+		delta = (now - lastTime) / nsBetweenTicks;
+
+		/* El 1 representa el 100% de 1/60 fps. Es decir que cuando el valor delta sea >= 1, entonces paso 1/60 de
+		 * segundo, lo que significa que el programa ahora puede llamar a tick(). Si el valor delta es menor a 1,
+		 * entonces solo tiene un porcentaje decimal de cuanto ha pasado del tiempo necesario. */
+		if (delta >= 1) {
 			lastTime = now;
 			return true;
 		} else return false;
